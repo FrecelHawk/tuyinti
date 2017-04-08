17f1d8852bf36c4700c257aa085df24ab83ac095
Hawk <fxlssion@gmail.com>
2017-04-08 15:18:45 +0800

    Add

---
+++ tuyinti/app/src/main/java/org/tuyinti/BaseActivity.java
@@ -0,0 +1,19 @@
+package org.tuyinti;
+
+
+/**
+ * Created by visitor on 2017/4/8.
+ */
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.xutils.x;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        x.view().inject(this);
+    }
+}
---
+++ tuyinti/app/src/main/java/org/tuyinti/LoginActivity.java
@@ -0,0 +1,21 @@
+package org.tuyinti;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import org.xutils.view.annotation.ContentView;
+
+/**
+ * Created by visitor on 2017/4/8.
+ */
+
+
+@ContentView(R.layout.login_activity)
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
